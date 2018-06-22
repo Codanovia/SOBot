35ad5d0386b2dc51e5d3367ac0cc2d8aac62a4c9
Codanovia <xero9606@gmail.com>
2018-06-22 10:53:04 +0200

    Add command cz!8ball

---
+++ commands/8ball.js
@@ -0,0 +1,31 @@
+exports.run = (client, message, params) => {
+  var eightBall = new Array();
+  eightBall[0] = 'Tak';
+  eightBall[1] = 'Nie';
+  eightBall[2] = 'Zapytaj ponownie';
+  eightBall[3] = 'Nie wiem';
+  eightBall[4] = 'Szacowania są dobre';
+  eightBall[5] = 'Nie rozumiem co napisałeś';
+  eightBall[6] = 'Myślę, że tak';
+  eightBall[7] = 'Na pewno tak';
+  eightBall[8] = 'Niestety, nie';
+  eightBall[9] = 'Coś czuję, że tak';
+  eightBall[10] = 'Czuję, że nie';
+  eightBall[11] = 'Absolutnie nie!';
+  eightBall[12] = 'Nie myśl o tym';
+
+  message.channel.send(`:8ball: **${eightBall[Math.floor(Math.random()*eightBall.length)]}**`)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "8ball",
+  description: "Pyta magicnzą kulę 8 o coś tam",
+  usage: "8ball (pytanie)"
+};
