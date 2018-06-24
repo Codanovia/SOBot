eedef0775d84ba703f71692530b4b28f89035da7
Codanovia <xero9606@gmail.com>
2018-06-24 23:11:09 +0200

    Updated command cz!ogar

--- commands/ogar.js
+++ commands/ogar.js
@@ -2,8 +2,9 @@ exports.run = (client, message, params) => {
   var ogar = new Array();
   ogar[0] = 'https://cdn.discordapp.com/attachments/422082830085128233/459828007113195530/ogar_1.png';
   ogar[1] = 'https://cdn.discordapp.com/attachments/422082830085128233/459694325979283468/ogar.png';
+  ogar[2] = 'https://cdn.discordapp.com/attachments/422082830085128233/456126905746456597/tele.png';
 
-  message.channel.send(${ogar[Math.floor(Math.random()*ogar.length)]});
+  message.channel.send(ogar[Math.floor(Math.random()*ogar.length)]);
 };
 
 exports.conf = {
