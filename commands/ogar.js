34ac00b5d090ee186fbda82c7e4b9d0dc7442880
Codanovia <xero9606@gmail.com>
2018-06-25 17:17:12 +0200

    Update command cz!ogar
    added another picture (thx Buster)

--- commands/ogar.js
+++ commands/ogar.js
@@ -3,6 +3,7 @@ exports.run = (client, message, params) => {
   ogar[0] = 'https://cdn.discordapp.com/attachments/422082830085128233/459828007113195530/ogar_1.png';
   ogar[1] = 'https://cdn.discordapp.com/attachments/422082830085128233/459694325979283468/ogar.png';
   ogar[2] = 'https://cdn.discordapp.com/attachments/422082830085128233/456126905746456597/tele.png';
+  ogar[3] = 'https://cdn.discordapp.com/attachments/422082830085128233/460823813899288577/karabin_2.png'
 
   message.channel.send(ogar[Math.floor(Math.random()*ogar.length)]);
 };
