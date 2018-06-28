8b2a3bee62ef261021fbbfeebe056c91814718b1
Codanovia <xero9606@gmail.com>
2018-06-29 01:50:05 +0200

    97th commit

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,4 @@
-module.exports = member => {
+module.exports = (client, member) => {
   console.log(`Użytkownik ${member.user.username} opuścił serwer ${member.guild.name}`);
   client.channels.get('461223372785844247').send(`Użytkownik ${member.user} opuścił Czacior. Mamy nadzieję że kiedyś powrócisz do nas. :cry: `)
 };
