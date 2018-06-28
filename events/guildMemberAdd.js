2e0b12a6b4179db847e244e22b82281cb817d566
Codanovia <xero9606@gmail.com>
2018-06-29 01:41:05 +0200

    nothing really

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
-module.exports = (client, member) => {
+module.exports = member => {
   console.log(`Użytkownik ${member.user.username} dołączył do serwera ${member.guild.name}`);
-  client.channels.get(client.channels.find('name', 'wchodzenie_wychodzenie').id).send(`Użytkownik ${member.user} dołączył do Czaciora! Witaj w klubie! :smile:\nJeśli jesteś z Polski wpisz komendę **cz!pl** na kanale <#461193606233849857>.\nIf you live outside Poland type **cz!notpl** on channel <#461193606233849857>.`);
+  //client.channels.get(client.channels.find('name', 'wchodzenie_wychodzenie').id).send(`Użytkownik ${member.user} dołączył do Czaciora! Witaj w klubie! :smile:\nJeśli jesteś z Polski wpisz komendę **cz!pl** na kanale <#461193606233849857>.\nIf you live outside Poland type **cz!notpl** on channel <#461193606233849857>.`);
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,4 @@
-module.exports = (member, client) => {
-  const guild = member.guild;
-  console.log(`Użytkownik ${member.user.username} opuścił serwer ${guild.name}`);
-  client.channels.get(client.channels.find('name', 'wchodzenie_wychodzenie').id).send(`Użytkownik ${member.user} opuścił Czacior. Mamy nadzieję że kiedyś powrócisz do nas. :cry: `)
+module.exports = member => {
+  console.log(`Użytkownik ${member.user.username} opuścił serwer ${member.guild.name}`);
+  //client.channels.get(client.channels.find('name', 'wchodzenie_wychodzenie').id).send(`Użytkownik ${member.user} opuścił Czacior. Mamy nadzieję że kiedyś powrócisz do nas. :cry: `)
 };
