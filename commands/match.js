b193026ae950f4119e40e9e5a5246487b0b94cdd
Codanovia <xero9606@gmail.com>
2018-08-03 22:47:13 +0200

    Updated match command

--- commands/match.js
+++ commands/match.js
@@ -5,14 +5,15 @@ exports.run = (client, message, params) => {
   lovePercentage[2] = '20%! :frowning:';
   lovePercentage[3] = '30%! :neutral_face:';
   lovePercentage[4] = '40%! :thinking:';
-  lovePercentage[5] = '50%! :wink:';
-  lovePercentage[6] = '60%! :slight_smile:';
-  lovePercentage[7] = '69%! :upside_down:';
+  lovePercentage[5] = '50%! :slight_smile:';
+  lovePercentage[6] = '60%! :wink:';
+  lovePercentage[7] = '69%! :rofl:';
   lovePercentage[8] = '70%! :relieved:';
   lovePercentage[9] = '80%! :kissing_heart:';
   lovePercentage[10] = '90%! :kissing_closed_eyes:';
   lovePercentage[11] = '100%! :heart_eyes:';
   lovePercentage[12] = '200%! :couple_with_heart:';
+  lovePercentage[13] = '100%! :heart_eyes:';
 
   message.channel.send(`:heart: Wskaźnik miłości między **${params[0]}** a **${params[1]}** to **${lovePercentage[Math.floor(Math.random()*lovePercentage.length)]}**`)
 };
