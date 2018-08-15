ada23bc5679c90da277af8f5f1bc6df894264221
Codanovia <xero9606@gmail.com>
2018-08-15 15:09:45 +0200

    Add owo command

---
+++ commands/owo.js
@@ -0,0 +1,16 @@
+exports.run = (client, message, params) => {
+  message.channel.send('<:owo:478157178990100490>');
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 1
+};
+
+exports.help = {
+  name: "owo",
+  description: "owo",
+  usage: "śo!owo"
+};
